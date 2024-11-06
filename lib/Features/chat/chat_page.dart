fdc4e9f65be28821a71a5a1c5bc7ed48fdb85d0b
mahmoud-elgamal22 <melgamel@std.mans.edu.eg>
2024-11-06 15:09:14 +0200

    create send message text field

--- lib/Features/chat/chat_page.dart
+++ lib/Features/chat/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -7,7 +8,88 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ChatPage'),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Mahmoud',
+            ),
+            Text(
+              'Last seen 11:28',
+              style: Theme.of(context).textTheme.labelLarge,
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Iconsax.call_calling),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Iconsax.trash),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+        child: Column(
+          children: [
+            const Spacer(),
+            Row(
+              children: [
+                Expanded(
+                  child: Card(
+                    color: Colors.transparent,
+                    elevation: 0.0,
+                    margin: const EdgeInsets.all(8),
+                    child: TextField(
+                      maxLines: 5,
+                      minLines: 1,
+                      decoration: InputDecoration(
+                        hintText: 'write a message',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(
+                            color: Colors.transparent,
+                            strokeAlign: 2,
+                            width: 20,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 18, vertical: 20),
+                        suffixIcon: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.emoji_emotions_outlined),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Iconsax.send_1,
+                                size: 26,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.camera_alt_outlined,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
