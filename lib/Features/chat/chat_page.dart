8dea8cf09a29561727101470df8b86e0f43f7649
mahmoud-elgamal22 <melgamel@std.mans.edu.eg>
2024-11-07 09:29:39 +0200

    create message card & read and edit

--- lib/Features/chat/chat_page.dart
+++ lib/Features/chat/chat_page.dart
@@ -35,7 +35,83 @@ class ChatPage extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
         child: Column(
           children: [
-            const Spacer(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 20,
+                // reverse: true,
+                physics: const BouncingScrollPhysics(),
+                padding: const EdgeInsets.all(4),
+                itemBuilder: (context, index) {
+                  return Row(
+                    mainAxisAlignment: index % 2 == 0
+                        ? MainAxisAlignment.end
+                        : MainAxisAlignment.start,
+                    children: [
+                     index % 2 == 0
+                        ?  IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.edit_note_outlined,
+                          color: Colors.grey,
+                          size: 28,
+                        ),
+                      ) : const SizedBox(),
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                            bottomLeft:
+                                Radius.circular(index % 2 == 0 ? 16.0 : 0.0),
+                            bottomRight:
+                                Radius.circular(index % 2 == 0 ? 0.0 : 16.0),
+                            topRight: const Radius.circular(16),
+                            topLeft: const Radius.circular(16),
+                          ),
+                        ),
+                        color: index % 2 == 0
+                            ? Theme.of(context).colorScheme.primaryContainer
+                            : Theme.of(context).colorScheme.secondaryContainer,
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Container(
+                            constraints: BoxConstraints(
+                              maxWidth: MediaQuery.sizeOf(context).width / 2.0,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                const Text(
+                                  'messages How Are You Mahmoud',
+                                  // style: Theme.of(context).textTheme.labelLarge,
+                                ),
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(
+                                      '11:30 pm',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .labelSmall,
+                                    ),
+                                    const SizedBox(width: 5),
+                                    index % 2 == 0
+                                        ? const Icon(
+                                            Icons.check_circle_outlined,
+                                            size: 18,
+                                            color: Colors.blueAccent,
+                                          )
+                                        : const SizedBox(),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
             Row(
               children: [
                 Expanded(
@@ -58,7 +134,7 @@ class ChatPage extends StatelessWidget {
                           ),
                         ),
                         contentPadding: const EdgeInsets.symmetric(
-                            horizontal: 18, vertical: 20),
+                            horizontal: 14, vertical: 16),
                         suffixIcon: Row(
                           mainAxisAlignment: MainAxisAlignment.end,
                           mainAxisSize: MainAxisSize.min,
@@ -70,8 +146,7 @@ class ChatPage extends StatelessWidget {
                             IconButton(
                               onPressed: () {},
                               icon: const Icon(
-                                Iconsax.send_1,
-                                size: 26,
+                                Icons.camera_alt_outlined,
                               ),
                             ),
                           ],
@@ -80,10 +155,13 @@ class ChatPage extends StatelessWidget {
                     ),
                   ),
                 ),
-                IconButton(
+                IconButton.filled(
+                  // splashColor: kPrimaryColor,
+                  // color: Colors.indigo,
                   onPressed: () {},
                   icon: const Icon(
-                    Icons.camera_alt_outlined,
+                    Iconsax.send_1,
+                    size: 26,
                   ),
                 ),
               ],
