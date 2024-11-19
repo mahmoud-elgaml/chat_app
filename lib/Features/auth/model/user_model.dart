da88b5e8699284bc75c47f6aed0abf11f2b1ab8d
mahmoud-elgamal22 <melgamel@std.mans.edu.eg>
2024-11-19 11:02:39 +0200

    setup firestore& usermodel

---
+++ lib/Features/auth/model/user_model.dart
@@ -0,0 +1,50 @@
+class UserModel {
+  final String name;
+  final String id;
+  final String email;
+  final String createdAt;
+  final String lastActivated;
+  final String pushToken;
+  final String about;
+  final bool online;
+  final String image;
+
+  UserModel({
+    required this.name,
+    required this.id,
+    required this.email,
+    required this.createdAt,
+    required this.lastActivated,
+    required this.pushToken,
+    required this.about,
+    required this.online,
+    required this.image,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      name: json['name'],
+      id: json['id'],
+      email: json['email'],
+      createdAt: json['created_at'],
+      lastActivated: json['last_activated'],
+      pushToken: json['pushToken'],
+      about: json['about'],
+      online: json['online'],
+      image: json['image'],
+    );
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'image': image,
+      'created_at': createdAt,
+      'last_activated': lastActivated,
+      'pushToken': pushToken,
+      'about': about,
+      'online': online,
+    };
+  }
+}
